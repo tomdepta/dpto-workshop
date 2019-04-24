0ce4766bf541ef1af4bd52c974ffa796533272dd
tdepta <tdepta@future-processing.com>
2019-04-24 10:51:12 +0200

    Adding API project with hardcoded list

---
+++ DPTO.Api/Controllers/Car.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DPTO.Api.Controllers
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime AddedOn { get; set; }
+    }
+}
---
+++ DPTO.Api/Controllers/CarsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DPTO.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        private readonly List<Car> _cars = new List<Car>
+        {
+            new Car {Id = 1, Name = "Opel", AddedOn = DateTime.Now},
+            new Car {Id = 2, Name = "Renault", AddedOn = DateTime.Now}
+        };
+
+        [HttpGet]
+        public List<Car> Get()
+        {
+            return _cars;
+        }
+        
+        [HttpGet("{id}")]
+        public Car Get(int id)
+        {
+            return _cars.FirstOrDefault(car => car.Id == id);
+        }
+        
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+    }
+}
