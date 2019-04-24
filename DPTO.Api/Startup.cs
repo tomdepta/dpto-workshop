08ab2e9539ef6ad638f80982e37ad2324968b928
tdepta <tdepta@future-processing.com>
2019-04-24 11:51:52 +0200

    Adding EF Core with initial migration

--- DPTO.Api/Startup.cs
+++ DPTO.Api/Startup.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DPTO.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -28,6 +30,7 @@ namespace DPTO.Api
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
+            services.AddDbContext<DptoContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
---
+++ DPTO.Infrastructure/DptoContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DPTO.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DPTO.Infrastructure
+{
+    public class DptoContext : DbContext
+    {
+        public DptoContext(DbContextOptions<DptoContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Car> Cars { get; set; }
+    }
+}
